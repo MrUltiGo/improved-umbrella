90bafec613f3e392f4945809516ef6a6bf44288f
stngularity <pro646229@gmail.com>
2023-04-30 23:40:43 +0300

    Update main.cpp
    define suka

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include "myh.h"
 
+#define suka 0;
+
 int main(int argc, char** argv) {
     Struct* asd = new Struct();
     asd->num = 1;
-    return 0;
+    return suka;
 }
